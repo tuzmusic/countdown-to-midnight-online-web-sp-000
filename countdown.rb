a293d2c186f73566aefa32503e6e46e1bb6f8b3c
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-09 20:18:47 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,6 +3,15 @@ def countdown(secs)
   while secs > 0
     puts "#{secs} SECOND(S)!"
     secs -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+#write your code here
+def countdown_with_sleep(secs)
+  while secs > 0
+    puts "#{secs} SECOND(S)!"
+    secs -= 1
     sleep(1)
   end
   "HAPPY NEW YEAR!"
